5acdb6ca13045fa4a36beac634d463c873192948
binshida-pk <binshidamuthuvallur@gmail.com>
2025-06-03 15:18:57 +0530

    prop

--- src/Component/Users.tsx
+++ src/Component/Users.tsx
@@ -141,20 +141,6 @@ type props = {
 
 const Users = ({ email, job, user }: props) => {
   return (
-    // <div className="grid w-full pl-50 ">
-    //   <div className="grid grid-cols-3 h-13 p-3 border-b-2  hover:bg-blue-100">
-    //     <div className="flex gap-3">
-    //       <input
-    //         id="default-checkbox"
-    //         type="checkbox"
-    //         value=""
-    //         className="w-4 h-4 mt-1 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 text-black"
-    //       />
-    //       User
-    //     </div>
-    //     <div>Email</div>
-    //     <div>Job</div>
-    //   </div>
     <div className="grid w-full  ">
       <div className="grid grid-cols-3 h-13 p-3 border-b-2  hover:bg-blue-100">
         <div className="flex gap-5">
